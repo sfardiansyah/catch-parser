8e8a8b3cf87d575a90e91af0c63bfca4c6b38928
Syahrul Ardiansyah <syahrul.findi@gmail.com>
2021-09-15 13:16:08 +0700

    Init Express

---
+++ README.md
@@ -0,0 +1,23 @@
+# Catch JSONL Parser
+
+## Prerequisites
+- [Node](https://nodejs.org/en/download/) v14
+- [Yarn](https://classic.yarnpkg.com/en/docs/install#install-via-npm) (Optional)
+
+## Installation
+```
+# if yarn installed
+yarn
+
+# or use npm
+npm install
+```
+
+## How to Use
+```
+# if yarn installed
+yarn start
+
+# or use npm
+npm run start
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const dotenv = require("dotenv")
+
+const app = express()
+
+const init = async () => {
+  dotenv.config()
+
+  const port = process.env.PORT || 3000
+
+  app.get("/", (req, res) => res.send("Hello from Catch JSONL Parser!"))
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`)
+  })
+}
+
+init()
