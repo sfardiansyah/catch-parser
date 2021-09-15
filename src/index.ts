9ba963b2ebb87d1956cf55af54fbe294df1e5599
Syahrul Ardiansyah <syahrul.findi@gmail.com>
2021-09-15 13:35:14 +0700

    Init eslint

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from 'express';
+import dotenv from 'dotenv';
 
-const app = express()
+const app = express();
 
 const init = async () => {
-  dotenv.config()
+  dotenv.config();
 
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 3000;
 
-  app.get("/", (req, res) => res.send("Hello from Catch JSONL Parser!"))
+  app.get('/', (req, res) => res.send('Hello from Catch JSONL Parser!'));
   app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`)
-  })
-}
+    // eslint-disable-next-line no-console
+    console.log(`Server is running at http://localhost:${port}`);
+  });
+};
 
-init()
+init();
