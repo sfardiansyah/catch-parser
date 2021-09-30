bc975137ecd3c65ce6f743db8abd8922dd399648
Syahrul Ardiansyah <syahrul.findi@gmail.com>
2021-09-30 23:49:18 +0700

    Remove web server

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
-import express from 'express';
 import dotenv from 'dotenv';
 
 import fetchAndTransform from './service';
 
 dotenv.config();
 
-const app = express();
-
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send('Hello from Catch JSONL Parser!'));
-
 fetchAndTransform();
-
-const server = app.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running at http://localhost:${port}`);
-});
-
-export default server;
